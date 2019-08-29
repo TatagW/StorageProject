5aac72479d747f8e5f83d152d625654dd56ea547
rizkyich <ichsandirizky@gmail.com>
2019-08-29 21:27:23 +0700

    almost done

--- routes/storageItemRoute.js
+++ routes/storageItemRoute.js
@@ -2,5 +2,5 @@ const router = require("express").Router()
 const StorageItemController = require("../controllers/storageItemController")
 
 router.get("/add", StorageItemController.create)
-router.get("/delete", StorageItemController.takeItem)
+router.post("/delete", StorageItemController.takeItem)
 module.exports = router
